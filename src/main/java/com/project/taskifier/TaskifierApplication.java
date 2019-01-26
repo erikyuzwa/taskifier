f54784bb71962eada2a028ba3cdf74b07cf33049
Erik Yuzwa <erikyuzwa@gmail.com>
2019-01-26 11:33:23 -0700

    initial commit of initializr zip

---
+++ src/main/java/com/project/taskifier/TaskifierApplication.java
@@ -0,0 +1,14 @@
+package com.project.taskifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskifierApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/project/taskifier/TaskifierApplicationTests.java
@@ -0,0 +1,17 @@
+package com.project.taskifier;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskifierApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
